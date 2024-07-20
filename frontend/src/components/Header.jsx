cccfef06e42657b4b39cf4a19cc39d938a0874e1
Kavishka <kkravishan2@gmail.com>
2024-07-20 20:32:21 +0530

    have to find error of FeaturedTourList

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+import "../css/header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container className="mt-3">
+      <Row>
+        <div className="nav_wrapper d-flex align-items-center justify-content-between">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="navigation">
+            <ul className="menu d-flex align-items-center gap-5">
+              <li className="nav__item">
+                <NavLink
+                  to="/"
+                  end
+                  className={({ isActive }) => (isActive ? "active__link" : "")}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) => (isActive ? "active__link" : "")}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink
+                  to="/tour"
+                  className={({ isActive }) => (isActive ? "active__link" : "")}
+                >
+                  Tours
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="nav_right d-flex align-items-center gap-4">
+            <div className="nav_btns d-flex align-items-center gap-4">
+              <Button className="btn secondary__btn">
+                <Link to="/login">Login</Link>
+              </Button>
+              <Button className="btn primary__btn">
+                <Link to="/register">Register</Link>
+              </Button>
+            </div>
+            <span className="mobile__menu">
+              <i className="ri-menu-line"></i>
+            </span>
+          </div>
+        </div>
+      </Row>
+    </Container>
+  );
 };
 
 export default Header;
